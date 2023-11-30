39caa9f4ee8fd864a81afa5c512fa0332704c0fa
CryptoFist <jonekell1002>
2023-11-30 15:56:20 +0100

    initial dev environment

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
+require("dotenv").config();
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.17",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 2000,
+                    },
+                },
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000,
+    },
+};
